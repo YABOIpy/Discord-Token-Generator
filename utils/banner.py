644d6f4d2edf7ed21b022d8ec050d785e9e44bd0
YABOI <110062350+YABOIpy@users.noreply.github.com>
2022-08-20 23:36:00 +0200

    Update banner.py

--- utils/banner.py
+++ utils/banner.py
@@ -5,7 +5,7 @@ def FlowBanner():
         f"""
    ___ _             ___           
 {colors.blue}| \033[39m| __| |_____ __ __/ __|___ _ _   {colors.blue}|                ${colors.white} YABOI {colors.blue}|{colors.white} payfloww
-{colors.blue}| \033[39m| _|| / _ \ V  V / (_ / -_) ' \  {colors.blue}|                ${colors.white} discord.gg{colors.blue}/{colors.white}dwncord
+{colors.blue}| \033[39m| _|| / _ \ V  V / (_ / -_) ' \  {colors.blue}|                ${colors.white} discord.gg{colors.blue}/{colors.white}downcord
 {colors.blue}| \033[39m|_| |_\___/\_/\_/ \___\___|_||_| {colors.blue}|
  
 {colors.blue}~{colors.white} Version{colors.blue}: v{colors.white}0.1
