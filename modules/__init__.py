ff87716116d4186f8de5cc44ad5c20535b207bd8
YABOI <110062350+YABOIpy@users.noreply.github.com>
2022-08-21 15:57:32 +0200

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,3 @@
 from modules.fingerprint import fingerprint
 from modules.random_email import getemail
-from modules.hcap import bypass #bypass("4c672d35-0701-42b2-88c3-78380b0db560", "discord.com", 'ca8jmszz7mflb00:VA71WECdzgcQrHb7@residential.proxyscrape.com:8080', 's')
+from modules.hcap import bypass #bypass("4c672d35-0701-42b2-88c3-78380b0db560", "discord.com", 'proxy', 's')
